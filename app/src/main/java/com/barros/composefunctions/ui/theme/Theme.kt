c219f36676a1c45b4b32af18b2edbc1453eab7b8
Barros9 <giu.barreca@gmail.com>
2022-07-10 10:37:14 +0200

    Used only LightColorPalette

--- app/src/main/java/com/barros/composefunctions/ui/theme/Theme.kt
+++ app/src/main/java/com/barros/composefunctions/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ fun ComposeFunctionsTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
